ee0b2a3f6f897e088168313cd5b417ca7ef6747f
Pascal Knupper <pascal.knupper@otto.de>
2025-06-12 11:25:02 +0200

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.otto.de/customer-registration/customerAccount');
+
+  await page.getByTestId('fti-registration-password-input').fill('1')
+  await page.getByTestId('fti-registration-password-repeat-input').fill('1')
+  await page.getByRole('button', { name: 'Weiter' }).click()
+
+  const viewports = [
+      { breakpoint: 'xl', width: 1920, height: 1080 },
+      { breakpoint: 's', width: 360, height: 1080 }
+  ];
+
+  for (const viewport of viewports) {
+        await page.setViewportSize({
+          height: viewport.height,
+          width: viewport.width
+        });
+      }
+
+});
